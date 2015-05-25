1f1da6aceef58b5e421ea710585dc24b6e46d430
LindsayElia <lindsay.elia@gmail.com>
2015-05-24 17:44:46 -0700

    calculator works for addition, subtraction, but not multiplication or division

--- myscript.js
+++ myscript.js
@@ -3,26 +3,51 @@ $(document).ready(function() {
 	console.log("document ready");
 
 	var screen = $("#screen");
-
-// not yet using
-	var total = 0;
-	var tempArr = [];
-	var prevButton;
-	var calcB = $("#calc");
+	var mostButtons = $(".buttons span").not("#cancel").not("#calc"); //select all items in the .buttons class that are also a <span>
+	// and do not have the id #calc or the id #cancel
+	var cancelButton = $("#cancel"); 			//select the item with the id #cancel
+	var euqalButton = $("#calc"); 				//select the item with the id #calc
 	
-//	var allButtons = $(".buttons");
-//	console.log(allButtons);
-
-
-	$(".buttons span").on("click", function(){	//select all items in the .buttons class that are also a <span>
+	
+	mostButtons.on("click", function(){	
 		//console.log("clicked a button");		//check that it's working as expected
 		var element = $(this);
 		var elementValue = element.text()
-		console.log(elementValue);
-		screen.text(elementValue);
+		console.log(elementValue);				//check that it's working as expected
+		//screen.text(elementValue);			//shows just the current button value in the screen
+		screen.append(elementValue);			//adds the current button value to the end of the HTML in the screen
 	});
+	
+	cancelButton.on("click", function(){
+		var elementCancel = $(this);
+		elementCancelValue = "clear";
+		console.log(elementCancelValue);
+		screen.text("");
+	});
+	
+	euqalButton.on("click", function(){
+		//get current value inside of screen
+		var screenInput = screen.text();
+		console.log(screenInput);
+			
+		//var thing = $.type(screenInput);
+		//console.log(thing);					//tells me that the screenInput is a string
+		
+		//convert string to numbers & operators & save in new variable
+		//var strToNum = parseInt(screenInput, 10);
+
+		//var thing2 = $.type(strToNum);
+		//console.log(thing2);					//tells me that the strToNum is a number
+		//console.log(strToNum);				//tells me that the parseInt is stopping at the operator
+		
+		//doMath();
+		var result = eval(screenInput);			//eval() is magic!!
+		console.log(result);					//will only work on plus and minues, though, not multiply or divide
+
+		//display result inside of screen, replace inputValue text()
+		screen.text(result);
+	})
 
 
 
 }); 	// closes the file
-	
